ee42f46c883100acfe015b948d010f91b542f8a7
Lazar Milovanovic <milazar002@gmail.com>
2026-01-26 16:11:05 +0100

    Feat: Tablet & Desktop Design

--- script/script.js
+++ script/script.js
@@ -15,37 +15,44 @@ function makeEl(elTag, elClass, elText) {
 
 const regions = [];
 async function getCountries() {
-  const response = await fetch("https://restcountries.com/v3.1/all?fields=name,region,capital,population,independent,flags");
-  const data = await response.json();
-  data.forEach((country) => {
-    if (country.independent) {
-      const countryCard = makeEl("div", "search-results__card");
-      const countryFlag = makeEl("img", "card__flag");
-      countryFlag.src = country.flags.png;
-      const cardInfo = makeEl("div", "card__info");
-      const countryName = makeEl("h2", "card__name", country.name.common);
-      const countryRegion = makeEl("p", "card__region", `Region: ${country.region}`);
-      const countryCapital = makeEl("p", "card__capital", `Capital: ${country.capital[0]}`);
-      const formatPopulation = new Intl.NumberFormat("en-US", {
-        notation: "compact",
-      }).format(country.population);
-      const countryPopulation = makeEl("p", "card__population", `Population: ${formatPopulation}`);
-      cardInfo.append(countryName, countryRegion, countryCapital, countryPopulation);
-      countryCard.append(countryFlag, cardInfo);
-      searchResults.append(countryCard);
-      if (regions.includes(country.region)) {
-        return;
-      } else {
-        regions.push(country.region);
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all?fields=name,region,capital,population,independent,flags");
+    const data = await response.json();
+    data.forEach((country) => {
+      if (country.independent) {
+        const countryCard = makeEl("div", "search-results__card");
+        const countryFlag = makeEl("img", "card__flag");
+        countryFlag.src = country.flags.png;
+        const cardInfo = makeEl("div", "card__info");
+        const countryName = makeEl("h2", "card__name", country.name.common);
+        const countryRegion = makeEl("p", "card__region", `Region: ${country.region}`);
+        const countryCapital = makeEl("p", "card__capital", `Capital: ${country.capital[0]}`);
+        const formatPopulation = new Intl.NumberFormat("en-US", {
+          notation: "compact",
+        }).format(country.population);
+        const countryPopulation = makeEl("p", "card__population", `Population: ${formatPopulation}`);
+        cardInfo.append(countryName, countryRegion, countryCapital, countryPopulation);
+        countryCard.append(countryFlag, cardInfo);
+        searchResults.append(countryCard);
+        if (regions.includes(country.region)) {
+          return;
+        } else {
+          regions.push(country.region);
+        }
+        console.log(regions);
+        regions.sort();
       }
-      console.log(regions);
-      regions.sort();
-    }
-  });
-  regions.forEach((region) => {
-    const regionOption = makeEl("option", undefined, region);
-    regionOption.value = region;
-    regionSelect.append(regionOption);
-  });
+    });
+    const allOption = makeEl("option", undefined, "All");
+    allOption.value = "All";
+    regionSelect.append(allOption);
+    regions.forEach((region) => {
+      const regionOption = makeEl("option", undefined, region);
+      regionOption.value = region;
+      regionSelect.append(regionOption);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 getCountries();
